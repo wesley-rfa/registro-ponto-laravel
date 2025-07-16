75bb2b8928a972861cbf845d5fc07e6bb3491e79
Wesley Ribeiro <wesley_ribeirof@outlook.com>
2025-07-16 10:00:23 -0300

    feat: criados middlewares para restrição de acesso

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+            'employee' => \App\Http\Middleware\EnsureUserIsEmployee::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
