603e68768c6db69c283a8e280275b793c0c31521
Wesley Ribeiro <wesley_ribeirof@outlook.com>
2025-07-16 01:23:26 -0300

    feat: criar tabelas do projeto com índices

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('cpf', 14)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('job_title', 100)->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('postal_code', 10)->nullable();
+            $table->string('address')->nullable();
+            $table->string('role', 20)->default('employee')->index();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
