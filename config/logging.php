5a51d044630948734a2fa60c8abad79b5573dad8
Wesley Ribeiro <wesley_ribeirof@outlook.com>
2025-07-17 01:51:18 -0300

    feat: criação de canal de log específico para os serviços de cep

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,14 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'cep' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/cep/cep.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+            'replace_placeholders' => true,
+        ],
+
     ],
 
 ];
